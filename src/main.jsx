5a50b3829e4d4fb2a12f49db1f5158f3f9f1e9fc
Celia Recio <celia.recio@yahoo.es>
2025-02-10 18:22:19 +0100

    first try to fix page to be published

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.scss'
-import './styles/App.scss'
-import App from './App.jsx'
-import { HashRouter } from 'react-router'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.scss';
+import './styles/App.scss';
+import App from './App.jsx';
+import { HashRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
